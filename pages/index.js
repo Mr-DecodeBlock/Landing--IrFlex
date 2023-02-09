82c84be689c735f8fa412d6405f89962f6b17608
Mr-DecodeBlock <decodeblock1010@gmail.com>
2023-02-09 11:24:31 -0500

    fork

--- pages/index.js
+++ pages/index.js
@@ -1,29 +1,29 @@
 import Head from "next/head";
-import FeaturesTwo from "../components/Features/FeaturesTwo";
-import HeroTwo from "../components/Hero/HeroTwo";
-import Cta from "../components/HomeTwo/Cta";
-import Interfaces from "../components/HomeTwo/Interfaces";
-import ManageData from "../components/HomeTwo/ManageData";
-import OurGoal from "../components/HomeTwo/OurGoal";
-import PricingContent from "../components/Pricing/PricingContent";
-import Services from "../components/Services/Services";
-import Testimonial from "../components/Testimonial/Testimonial";
+import FaqContent from "../components/Faq/FaqContent";
+import HeroThree from "../components/Hero/HeroThree";
+import ContentBlockOne from "../components/HomeThree/ContentBlockOne";
+import ContentBlockThree from "../components/HomeThree/ContentBlockThree";
+import ContentBlockTwo from "../components/HomeThree/ContentBlockTwo";
+import Cta from "../components/HomeThree/Cta";
+import PricingContentTwo from "../components/Pricing/PricingContentTwo";
+import FeaturesFour from "./../components/Features/FeaturesFour";
+import TestimonialTwo from "./../components/Testimonial/TestimonialTwo";
 
-export default function Home() {
+export default function indexThree() {
 	return (
 		<>
 			<Head>
-				<title>Xmoze - SaaS Multipurpose Landing Template 2</title>
+				<title>Xmoze - SaaS Multipurpose Landing Template 3</title>
 			</Head>
-			<HeroTwo />
-			<FeaturesTwo />
-			<OurGoal />
-			<Interfaces />
-			<Services />
-			<ManageData />
-			<PricingContent />
-			<Testimonial />
+			<HeroThree />
+			<FeaturesFour />
 			<Cta />
+			<ContentBlockOne />
+			<ContentBlockTwo />
+			<ContentBlockThree />
+			<PricingContentTwo />
+			<TestimonialTwo />
+			<FaqContent />
 		</>
 	);
 }
